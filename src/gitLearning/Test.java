7bd97062f6f1bb90c2fe3547b7e077d0698368c8
gaoc10 <runharry@163.com>
2017-11-21 22:37:18 +0800

    Update Test.java

--- src/gitLearning/Test.java
+++ src/gitLearning/Test.java
@@ -5,5 +5,8 @@ public class Test {
 		System.out.println("hello");
 		System.out.println("hhh==dfdmfekjghh");
 	}
+	public static void main(String[] args){
+		System.out.println("hello,world");
+	}
 
 }
