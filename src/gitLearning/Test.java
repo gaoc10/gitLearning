42b7abedb3a94951219a503543d2ff4009e316bf
gaoc10 <runharry@163.com>
2017-11-21 22:29:26 +0800

    fdkjf

--- src/gitLearning/Test.java
+++ src/gitLearning/Test.java
@@ -3,7 +3,7 @@ package gitLearning;
 public class Test {
 	public static void name() {
 		System.out.println("hello");
-		System.out.println("hhhhh");
+		System.out.println("hhhhh==dfjekljfgl");
 	}
 
 }
